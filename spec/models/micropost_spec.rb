fe2800f1866e562bd6c76e27373c0593d87134a9
abbr <wenfred@gmail.com>
2014-01-25 19:29:01 -0800

    10.1.3 begin

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -4,7 +4,7 @@ describe Micropost do
 
   let(:user) { FactoryGirl.create(:user) }
   before do
-    # This code is not idiomatically correct.
+  # This code is not idiomatically correct.
     @micropost = Micropost.new(content: "Lorem ipsum", user_id: user.id)
   end
 
@@ -12,4 +12,10 @@ describe Micropost do
 
   it { should respond_to(:content) }
   it { should respond_to(:user_id) }
+  it { should be_valid }
+
+  describe "when user_id is not present" do
+    before { @micropost.user_id = nil }
+    it { should_not be_valid }
+  end
 end
