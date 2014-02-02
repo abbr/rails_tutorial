e3c5b9f472c495cba9d264ce64d5f9496edb9fe4
abbr <wenfred@gmail.com>
2014-02-01 18:49:35 -0800

    11.3.2 begin

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,10 +24,8 @@ class User < ActiveRecord::Base
     Digest::SHA1.hexdigest(token.to_s)
   end
   def feed
-    # This is preliminary. See "Following users" for the full implementation.
-    Micropost.where("user_id = ?", id)
-  end
-  
+    Micropost.from_users_followed_by(self)
+  end  
   def following?(other_user)
     relationships.find_by(followed_id: other_user.id)
   end
